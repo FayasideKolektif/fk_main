61bd8bc3fce03538b086758a2a0fa471130afca8
FayasideKolektif <142313817+FayasideKolektif@users.noreply.github.com>
2023-08-18 09:00:09 +0200

    Update periodic.py

--- periodic.py
+++ periodic.py
@@ -1,15 +1,11 @@
+
 import requests
-import schedule
-import time
 
 def make_request():
-    url = "https://www.defayasitekolektif.org"  # Replace with your target URL
+    url = "https://www.defayasitekolektif.org"
     response = requests.get(url)
     print("Request made:", response.status_code)
 
-# Schedule the make_request function to run every 15 minutes
-schedule.every(15).minutes.do(make_request)
+if __name__ == "__main__":
+    make_request()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
